23e8ee45ce6e938cba32ddb7e712a053168339f5
ershuo <123456@qq.com>
2022-05-12 10:43:12 +0800

    11-Thymeleaf-04-Literals（字面量）

---
+++ 11-Thymeleaf-04-Literals/src/main/java/com/fc/controller/BooleanController.java
@@ -0,0 +1,26 @@
+package com.fc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+
+@Controller
+public class BooleanController {
+    @RequestMapping("boolean")
+    public String test(Model model) {
+        model.addAttribute("boolean", true);
+        model.addAttribute("age", 22);
+
+        ArrayList<String> notes = new ArrayList<>();
+
+        notes.add("美食1：今天吃了饭");
+        notes.add("美食2：今天吃了饭");
+        notes.add("美食3：今天吃了饭");
+
+        model.addAttribute("notes", notes);
+
+        return "boolean";
+    }
+}
---
+++ 11-Thymeleaf-04-Literals/src/main/java/com/fc/controller/NullController.java
@@ -0,0 +1,16 @@
+package com.fc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class NullController {
+@RequestMapping("null")
+    public String test(Model model){
+        model.addAttribute("value",null);
+        model.addAttribute("String","");
+
+        return "null";
+    }
+}
